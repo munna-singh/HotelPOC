f6e1679faea7b88f00af39e6a2e6d56b87fc713c
ayyadurai <ayyadurai.vellaichamy@galepartners.com>
2015-11-06 10:45:42 +0530

    Updated booking page

---
+++ HotelBooking/Manager/RetrievePnrDetails.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using Common.Sabre.Hotels.TravelItineraryReadInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public class RetrievePnrDetails
+    {
+        public TravelItineraryReadRS GetDetails(string pnrIdentifier)
+        {
+            var session = SabreSessionManager.Create();
+
+            var t = new ReadTravelerInfo().ReadInfo(session.SecurityValue.BinarySecurityToken, pnrIdentifier);
+
+            SessionClose close = new SessionClose();
+            close.Close(session.SecurityValue.BinarySecurityToken);
+
+            return t;
+        }
+    }
+}
