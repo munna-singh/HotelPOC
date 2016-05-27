ff0e6bcd1d88e48579573ace719e6aa16d58c846
Mujeeb <mujeeb.ks@gmail.com>
2016-05-27 15:52:45 +0530

    Hotel Beds POC Codes

---
+++ HotelBooking/HotelBeds/Provider/HotelBedsConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBeds.Provider
+{
+    public class HotelBedsConstants
+    {
+        public static int TotalAdults = 1;
+
+        public static int NightsDuration = 30;
+
+        public static int RadiusLimit = 60;
+
+        public static int AdultAge = 30;
+
+        public static char SpaceSeperator = ' ';
+    }
+}
