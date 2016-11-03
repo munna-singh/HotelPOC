764c6e9afa1b314418c64be64cd48e81eeccffe1
Vishwajeet Manish <vishwajeet.manish@galepartners.com>
2016-11-03 12:40:50 +0530

    Missing Files committed for Car POC for Tourico.

---
+++ HotelBooking/UI/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using TE.Core.ServiceCatalogues.HotelCatalog.Provider;
+using Newtonsoft.Json;
+using TE.Core.Providers.Tourico.Hotel.Provider;
+
+
+namespace UI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/<controller>
+        public  HotelAvailabilityProviderRes Get()
+        {
+
+            TouricoHotelSearchProvider searchProvider = new TouricoHotelSearchProvider();
+
+            HotelAvailabilityProviderReq request = new HotelAvailabilityProviderReq();
+
+            request.CheckInDate = DateTime.Now.AddDays(3).Date;
+            request.CheckOutDate = DateTime.Now.AddDays(5).Date;
+
+            request.HotelCodes = new System.Collections.Generic.List<string> { "8198" };
+            //TODO: 
+            //request.TotalAdults = 2;
+
+            HotelAvailabilityProviderRes res = searchProvider.Search(request);
+           
+            return res;
+        }
+
+      
+    }
+}
